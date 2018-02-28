9528f394ec94e546aac7b741eed742fbd8c713d2
cjredmond <connor.redmond@gmail.com>
2018-02-28 15:09:33 -0500

    initial

---
+++ tweet_grader/playground/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from textblob import TextBlob
+import re
+
+def clean_tweet(tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+|([^0-9A-Za-z \t])|(\w+:\/\/\S+))", " ", tweet).split())
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['rating'] = 'None'
+        if self.request.GET:
+            data = self.request.GET
+            tweet = data['tweet']
+            print(tweet)
+            analysis = TextBlob(clean_tweet(tweet))
+            context['rating'] = analysis.sentiment.polarity
+        return context
