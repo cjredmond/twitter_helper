3db3a0baa571aa59bf409f7a60b13c2a890982d0
cjredmond <connor.redmond@gmail.com>
2018-03-01 12:41:36 -0500

    synced react

--- tweet_grader/frontend/src/App.js
+++ tweet_grader/frontend/src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Enter a tweet</p>
+        <form class="" action="" method="get">
+          <input type="text" name="tweetreact"/>
+          <input type="submit" name="" value="Submit"/>
+        </form>
       </div>
     );
   }
